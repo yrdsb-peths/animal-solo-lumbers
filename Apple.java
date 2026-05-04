8f4418ea0a43f735842ab1a0f4452fe04e473556
lumbers <noahlum2009@gmail.com>
2026-05-04 17:13:52 -0400

    added title world

--- Apple.java
+++ Apple.java
@@ -12,9 +12,13 @@ public class Apple extends Actor
      * Act - do whatever the Apple wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+    
     public void act()
     {
         // Add your action code here.
+        GreenfootImage image = new GreenfootImage("apple2.png");
+        image.scale(50, 50);
+        setImage(image);
         int x = getX();
         int y = getY() + 1;
         setLocation(x,y);
