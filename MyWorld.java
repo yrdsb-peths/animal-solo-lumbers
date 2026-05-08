1145053b993d3d5cd4d01be13d6e02ba7d1ac5da
lumbers <noahlum2009@gmail.com>
2026-05-08 11:54:58 -0400

    added world comments

--- MyWorld.java
+++ MyWorld.java
@@ -6,24 +6,28 @@ public class MyWorld extends World {
     int level = 1;
     public MyWorld() {
         super(600, 400, 1, false);
-        
+        //creates new elephant
         Elephant ele = new Elephant();
         addObject(ele, 300, 300);
         
+        //creates a label for score
         scoreLabel = new Label(0, 80);
         addObject(scoreLabel, 50, 50);
         
+        //references the apple method 
         createApple();
     }
     
     public void gameOver()
     {
+        //creates a new label after you die, which is game over
         Label gameOverLabel = new Label("Game Over", 100);
         addObject(gameOverLabel, 300, 200);
     }
     
     public void increaseScore()
     {
+        //increases score and changes the label to match
         score++;
         scoreLabel.setValue(score);
         
@@ -34,8 +38,10 @@ public class MyWorld extends World {
     }
     
     public void createApple()
-    {      
+    {   
+        //creates a new apple object
         Apple apple = new Apple();
+        //changes the speed of the apple falling every 5 seconds
         apple.setSpeed(level);
         int x = Greenfoot.getRandomNumber(600);
         int y = 0;
